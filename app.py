1d4f77f5f281d7f7514186f5fd76ec60d52df4b9
Jaeho Jung <iwogh3176@gmail.com>
2022-03-08 16:48:19 +0900

    메인 브랜치 병합

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+
 # 로그인 페이지
 @app.route('/login')
 def login():
@@ -50,6 +51,7 @@ def login():
     msg = request.args.get("msg")
     # 로그인 페이지로 이동
     return render_template('login.html', msg=msg)
+
 
 # 로그인
 @app.route('/sign_in', methods=['POST'])
@@ -66,10 +68,7 @@ def sign_in():
         # 페이로드에 아이디 및 토큰 파기 시간 저장
         payload = {
             'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-            # 'exp': datetime.utcnow() + timedelta(seconds=60 * 5)  # 로그인 5분 유지
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(minutes=60)  # 로그인 5분 유지
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24),  # 로그인 24시간 유지
         }
         # 토큰 생성(암호화)
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -78,6 +77,7 @@ def sign_in():
     # 해당 정보가 없을 경우
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 # 회원가입
 @app.route('/sign_up/save', methods=['POST'])
@@ -100,6 +100,7 @@ def sign_up():
     # 회원가입 성공 반환
     return jsonify({'result': 'success'})
 
+
 # 회원가입 시 아이디 중복 확인
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
@@ -109,11 +110,13 @@ def check_dup():
     # 사용자 아이디 존재여부 반환
     return jsonify({'result': 'success', 'exists': exists})
 
+
 # 회원가입 시 닉네임 중복 확인 - ( 현재 미작동 )
 def check_dup_nick():
     nickname_receive = request.form['nickname_give']
     exists = bool(db.users.find_one({"nickname": nickname_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
 
 # 오늘 나의 계획을 등록하는 API입니다. (메인 페이지의 ajax가 콜합니다.)
 @app.route('/POST/plan', methods=['POST'])
@@ -161,6 +164,7 @@ def post_plan():
 
     except jwt.ExpiredSignatureError:
         return redirect(url_for("/"))
+
 
 # 세부 페이지 계획 포스팅 인덱스별 접속
 @app.route('/detail/<plan_no>')
@@ -184,58 +188,12 @@ def detail(plan_no):
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+
 # 세부 페이지에 인덱스 없이 접속할 경우 home() 함수를 호출
 @app.route('/detail')
 def detail_none():
     return home()
 
-
-# 오늘 나의 계획을 등록하는 API입니다. (메인 페이지의 ajax가 콜합니다.)
-@app.route('/POST/plan', methods=['POST'])
-def post_plan():
-    # 토큰 가져오기
-    token_receive = request.cookies.get('mytoken')
-    # 토큰이 유효한 경우에만 아래 처리 실행
-    try:
-        # 토큰을 복호화
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 유저DB에서 토큰["id"]를 키로 유저검색
-        user_info = db.users.find_one({"username": payload["id"]})
-
-        my_plan_receive = request.form['myPlan_give'] # 계획
-        registration_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 등록시간 (초단위까지)
-        today = datetime.now().strftime('%Y-%m-%d')  # 등록시간 (년월일)
-
-        # [플랜 고유번호 부여]
-        # plans DB에서 오늘 날짜로 등록된 전체 데이터 조회
-        today_all_plans = list(db.plans.find({'today': today}, {'_id': False}))
-
-        # 오늘 등록된 플랜이 하나도 없는 경우
-        if len(today_all_plans) == 0:
-            # 플랜 번호 1을 부여
-            plan_no = 1
-        # 등록된 플랜이 한 개라도 있는 경우
-        else:
-            # 람다함수를 이용, 플랜 번호를 key로 오름차순 정률 후 [-1]로 마지막 요소 추출
-            last_plan = sorted(today_all_plans, key=lambda k: k['plan_no'])[-1]
-            # 최근 등록된 플랜 번호에 1을 더해 플랜 번호 부여
-            plan_no = last_plan['plan_no'] + 1
-
-        doc = {
-            'plan_no': plan_no,  # 고유번호
-            'username': user_info['username'],  # 유저 아이디
-            'nickname': user_info['nickname'],  # 유저 닉네임
-            'my_plan': my_plan_receive,  # 계획
-            'registration_time': registration_time,  # 등록시간 (초단위까지)
-            'today': today  # 오늘 날짜
-        }
-        # myPlan DB에 유저의 계획 등록
-        db.plans.insert_one(doc)
-        # json형태로 response 반환
-        return jsonify({'result': 'success', 'msg': '오늘의 계획이 등록되었습니다!'})
-
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("/"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
